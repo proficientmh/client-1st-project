71e901f33b1bbab6be3cbb02215b41e2cab504af
proficientmh <87502934+proficientmh@users.noreply.github.com>
2021-08-05 00:33:36 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal({backdrop: true});
+  });
+  $("#myBtn1").click(function(){
+    $("#myModal").modal({backdrop: true});
+  });
+  $("#myBtn2").click(function(){
+    $("#myModal").modal({backdrop: true});
+  });
+  /*1st*/
+
+  $("#myBtn3").click(function(){
+    $("#myModal2").modal({backdrop: true});
+  });
+  $("#myBtn4").click(function(){
+    $("#myModal2").modal({backdrop: true});
+  });
+  $("#myBtn5").click(function(){
+    $("#myModal2").modal({backdrop: true});
+  });
+/*2nd*/
+  $("#myBtn6").click(function(){
+    $("#myModal3").modal({backdrop: true});
+  });
+  $("#myBtn7").click(function(){
+    $("#myModal3").modal({backdrop: true});
+  });
+  $("#myBtn8").click(function(){
+    $("#myModal3").modal({backdrop: true});
+  });
+  /*3rd*/
+    $("#myBtn9").click(function(){
+    $("#myModal4").modal({backdrop: true});
+  });
+  $("#myBtn10").click(function(){
+    $("#myModal4").modal({backdrop: true});
+  });
+  $("#myBtn11").click(function(){
+    $("#myModal4").modal({backdrop: true});
+  });
+  /*4th*/
+    $("#myBtn12").click(function(){
+    $("#myModal4").modal({backdrop: true});
+  });
+
+  /*5th*/
+    $("#myBtn13").click(function(){
+    $("#myModal6").modal({backdrop: true});
+  });
+    $("#myBtn14").click(function(){
+    $("#myModal6").modal({backdrop: true});
+  });
+    $("#myBtn15").click(function(){
+    $("#myModal6").modal({backdrop: true});
+  });
+    $("#myBtn16").click(function(){
+    $("#myModal6").modal({backdrop: true});
+  });
+ /* $("#myBtn3").click(function() {
+  $("#myModal").remove()
+})*/
+
+
+
+
+
+/*$("#myBtn3").click(function() {
+  $("#myModal").remove()
+})
+$("#myBtn4").click(function() {
+  $("#myModal").remove()
+})
+$("#myBtn5").click(function() {
+  $("#myModal").remove()
+})*/
+/*modal 2*/
+/*$("#myBtn6").click(function() {
+  $("#myModal2").remove()
+})
+$("#myBtn7").click(function() {
+  $("#myModal2").remove()
+})
+$("#myBtn8").click(function() {
+  $("#myModal2").remove()
+})*/
+/*modal 3*/
+/*$("#myBtn9").click(function() {
+  $("#myModal3").remove()
+})
+$("#myBtn10").click(function() {
+  $("#myModal3").remove()
+})
+$("#myBtn11").click(function() {
+  $("#myModal3").remove()
+})
+$("#myBtn12").click(function() {
+  $("#myModal3").remove()
+})*/
+
+/*modal 4*/
+/*$("#myBtn13").click(function() {
+  $("#myModal4").remove()
+})
+$("#myBtn14").click(function() {
+  $("#myModal4").remove()
+})
+$("#myBtn15").click(function() {
+  $("#myModal4").remove()
+})
+$("#myBtn16").click(function() {
+  $("#myModal4").remove()
+})
+
+$(".close").click(function() {
+  $(".modal-backdrop,").remove()
+})
+$(".close").click(function() {
+  $(".modal").remove()
+})*/
+
+});
